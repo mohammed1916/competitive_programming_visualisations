e4734f7133e234031b87db885bc5555e9bccd8a5
Mohammed Abdullah <mohammed.a@bigbangboom.com>
2026-05-16 12:22:17 +0530

    chore: include monaco deps in optimizeDeps for Vite

--- visualizer/vite.config.js
+++ visualizer/vite.config.js
@@ -4,6 +4,9 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  optimizeDeps: {
+    include: ['@monaco-editor/react', 'monaco-editor'],
+  },
   server: {
     port: 3010,
   },
